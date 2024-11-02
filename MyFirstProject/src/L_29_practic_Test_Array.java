c3028178fbc1969df540348acf8e1c3fa4962acd
Sushant Kushwaha <shushantk2009@gmail.com>
2024-11-02 16:15:06 +0530

    Array practic

--- MyFirstProject/src/L_29_practic_Test_Array.java
+++ MyFirstProject/src/L_29_practic_Test_Array.java
@@ -1,12 +1,55 @@
 public class L_29_practic_Test_Array {
     public static void main(String[] args) {
         // PROBLEM:--1
-        float [] prise={25.25564f,258.23241f,892.51254f,544.265f,654.654452f};
-        float sum=0;
-        for(float element:prise){
-            sum =sum+element;
-            System.out.println(element);
+//        float [] prise={25.25564f,258.23241f,892.51254f,544.265f,654.654452f};
+//        float sum=0;
+//        for(float element:prise){
+//            sum =sum+element;
+//            System.out.println(element);
+//        }
+//        System.out.println("The Value Of Sum is :--"+sum);
+
+        //PROBLEM :---2
+//        float [] prise={25.25564f,258.23241f,892.51254f,544.265f,654.654452f};
+//        float num=25.25564f;
+//        boolean isInArray=false;
+//        for(float element:prise){
+//            if (num==element) {
+//            isInArray=true;
+//            break;
+//            }
+//        }
+//        if (isInArray){
+//            System.out.println("The Value is present in The Array");
+//        }
+//        else {
+//            System.out.println("The Value is not present in Array");
+//        }
+
+        //PORNLEM :---3
+//        int [] marks={65,75,98,47,56,85,74,58,69,25,89};
+//        int sum=0;
+//        System.out.println(marks.length);
+//        for (int element:marks){
+//            sum=sum+element;
+//        }
+//        System.out.println("The value of avrage marks is :--"+sum/marks.length);// marks.length avrage nikalne ke liye use kiya hu
+
+
+        //PORNLEM :---4 nahi samajh aaya
+
+
+        //PORNLEM :---5
+        int arr[]={2,5,7,8,9,3,5,7,5,8,5,65};
+        int n= Math.floorDiv(arr.length,2); // इसका अर्थ है कि arr.length को 2 से भाग दिया जाएगा।
+        for (int i=0;i<n;i++){
+                    //Swap a[i] and a[arr.length-1-i]
+                    //Swap ka formula
+
         }
-        System.out.println("The Value Of Sum is :--"+sum);
+
+
+
+
     }
 }
