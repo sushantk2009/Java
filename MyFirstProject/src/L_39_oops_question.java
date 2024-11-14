ebbb5e1e13943c2c06a1ac429ac394886ef84a35
Sushant Kushwaha <shushantk2009@gmail.com>
2024-11-14 11:20:05 +0530

    oops question

--- MyFirstProject/src/L_39_oops_question.java
+++ MyFirstProject/src/L_39_oops_question.java
@@ -1,24 +1,81 @@
 public class L_39_oops_question {
-    class Employee{
+    static class Employee {  // Make Employee static
         int Salary;
         String Name;
-        public int getSalary(){
 
+        public int getSalary() {
             return Salary;
         }
 
-        public String getName(){
-
+        public String getName() {
             return Name;
         }
-        public void setName(String n){
-            Name=n;
-        }
-    }
-    public void main(String[] args) {
-        Employee Sushant=new Employee();
-        Sushant.setName("My name is Sushant");
-        Sushant.Salary=3565;
 
+        public void setName(String n) {
+            Name = n;
+        }
+
+        static class CellPone {
+            public void Ringing() {
+                System.out.println("Ringing...");
+            }
+            public void Vibrating() {
+                System.out.println("Vibrating....");
+            }
+        }
+        static class square{
+        int side;
+        public int area(){
+
+            return side*side;
+        }
+        public int parimeter(){
+            return 4*side;
+        }
+        }
+        static class rectangle{
+            int length;
+            int width;
+            public int area(){
+                return length*width;
+            }
+            public int parimeter(){
+
+                return 2*(length+width);
+            }
+        }
+
+
+
+        public static void main(String[] args) {
+            //PROBLEM:1
+            Employee Sushant = new Employee();  // Directly create Employee instance
+            Sushant.setName("My name is Sushant");
+            Sushant.Salary = 3565;  // Directly assign Salary (public access)
+            System.out.println(Sushant.getSalary());
+            System.out.println(Sushant.getName());
+
+
+            //PROBLEM:2
+            CellPone phone = new CellPone();
+            phone.Ringing();
+            phone.Vibrating();
+
+            //PROBLEM:3
+            square sq= new square();
+            sq.side=3;
+            System.out.println("Area of The Square : "+sq.area());
+            System.out.println(("Parimeter of The Square is : "+sq.parimeter()));
+
+            //PROBLEM:4
+            rectangle re= new rectangle();
+            re.length=2;
+            re.width=3;
+            System.out.println("Area of Rectangle : "+re.area());
+            System.out.println("Parimete of Rectangle : "+re.parimeter());
+
+
+
+        }
     }
 }
