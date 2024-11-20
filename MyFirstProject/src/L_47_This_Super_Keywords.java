3d5a4d36161e7c6f91a5e61788574eb2f4cafba3
Sushant Kushwaha <shushantk2009@gmail.com>
2024-11-20 17:13:33 +0530

    Super and This keywords

---
+++ MyFirstProject/src/L_47_This_Super_Keywords.java
@@ -0,0 +1,34 @@
+class EkClass{
+    int a;
+    public int getA(){
+        return a;
+    }
+
+
+    EkClass(int v){
+        a=v;
+    }
+
+
+
+    public int returnone(){
+        return 1;
+    }
+}
+class DoClass extends EkClass{
+
+    DoClass(int c){
+        super(c);
+        System.out.println("This is an constructor of DoClass : ");
+
+    }
+}
+
+public class L_47_This_Super_Keywords {
+    public static void main(String[]args){
+        EkClass e=new EkClass(25);
+        System.out.println(e.getA());
+        DoClass d=new DoClass(45);
+        System.out.println(e.getA());
+    }
+}
